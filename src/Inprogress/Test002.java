2f3b49473322d4fc5df9a9903e67d60ae1a46a5d
Tarik <turgayakcay@gmail.com>
2023-02-22 19:44:47 +0100

    Added new lines in Test002

--- src/Inprogress/Test002.java
+++ src/Inprogress/Test002.java
@@ -5,5 +5,7 @@ public class Test002 {
     // work in progress
     public static void main(String[] args) {
         System.out.println("done");
+
+        System.out.println("Keeps history");
     }
 }
